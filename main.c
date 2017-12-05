ec21872c317a42d5b490a1ff815dbb61cf5886ed
postaron <florent.denef@gmail.com>
2017-12-05 17:39:40 +0100

    init

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
 
-int main(void)
-{
-    puts("Hello world !");
-    return EXIT_SUCCESS;
+int main(int argc, char *argv[]) {
+	int n, c;
+	if (argc != 3) {
+		fprintf(stderr,
+				"Erreur, nombre d'argument insuffisant.\nDeux sont nécessaires.\n");
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(argv[1]), c = atoi(argv[2]);
+	return EXIT_SUCCESS;
 }
--- makefile
+++ makefile
@@ -1,11 +1,11 @@
 CC=gcc
-CFLAGS=-pipe -ansi -W -Wall -Wextra -pedantic -fmessage-length=0
+CFLAGS=-pipe -ansi -W -Wall -Wpadded -Wextra -pedantic -fmessage-length=0
 LDFLAGS=-pipe
 SRC=$(wildcard *.c)
 OBJ=$(SRC:.c=.o)
 DEP=$(wildcard *.h)
 DEBUG=
-EXEC=projet
+EXEC=Election
 
 ifeq ($(DEBUG), yes)
 	CFLAGS += -g3
